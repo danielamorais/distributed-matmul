712d1ba6b7cf7abbdfd7ed5dffb56cb19d8cde60
Arthur Barata <arthurpbarata@gmail.com>
2025-03-16 20:51:24 -0300

    change controller test

--- testing/mininet_docker.py
+++ testing/mininet_docker.py
@@ -1,6 +1,6 @@
 import logging
 from mininet.net import Containernet
-from mininet.node import Docker, Controller
+from mininet.node import DefaultController
 from mininet.cli import CLI
 from mininet.link import TCLink
 
@@ -8,15 +8,13 @@ from mininet.link import TCLink
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def create_network():
-    net = Containernet(controller=Controller, link=TCLink)
-
-    # Controlador
-    net.addController('c0')
+    net = Containernet(controller=DefaultController, link=TCLink)
+    net.addController('c0', ip='127.0.0.1', port=6653)
 
     # Hosts Docker (nomes curtos)
-    dana_main = net.addDocker('dana', ip='10.0.0.2', dimage="distributed-matmul-dana-main")
-    remote1 = net.addDocker('remote1', ip='10.0.0.3', dimage="distributed-matmul-dana-remote-1")
-    remote2 = net.addDocker('remote2', ip='10.0.0.4', dimage="distributed-matmul-dana-remote-2")
+    dana_main = net.addDocker('dana', ip='10.0.0.251', dimage="distributed-matmul-dana-main")
+    remote1 = net.addDocker('remote1', ip='10.0.0.252', dimage="distributed-matmul-dana-remote-1")
+    remote2 = net.addDocker('remote2', ip='10.0.0.253', dimage="distributed-matmul-dana-remote-2")
     # locust = net.addHost('locust', cls=Docker, ip='10.0.0.5', dimage="distributed-matmul-locust-test")
     # serial = net.addHost('serial', cls=Docker, ip='10.0.0.6', dimage="distributed-matmul-serial-matmul")
     # locust_serial = net.addHost('locust-ser', cls=Docker, ip='10.0.0.7', dimage="distributed-matmul-locust-test-serial")
