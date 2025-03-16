253567cb0779cbb9ff8694dd7402e1a712c25869
Arthur Barata <arthurpbarata@gmail.com>
2025-03-16 20:46:28 -0300

    simplifying mininet

--- testing/mininet_docker.py
+++ testing/mininet_docker.py
@@ -11,10 +11,10 @@ def create_network():
     net = Containernet(controller=Controller, link=TCLink)
 
     # Controlador
-    c0 = net.addController('c0', ip='127.0.0.1', port=6653)
+    net.addController('c0')
 
     # Hosts Docker (nomes curtos)
-    dana_main = net.addDocker('dana-main', ip='10.0.0.2', dimage="distributed-matmul-dana-main")
+    dana_main = net.addDocker('dana', ip='10.0.0.2', dimage="distributed-matmul-dana-main")
     remote1 = net.addDocker('remote1', ip='10.0.0.3', dimage="distributed-matmul-dana-remote-1")
     remote2 = net.addDocker('remote2', ip='10.0.0.4', dimage="distributed-matmul-dana-remote-2")
     # locust = net.addHost('locust', cls=Docker, ip='10.0.0.5', dimage="distributed-matmul-locust-test")
@@ -26,9 +26,9 @@ def create_network():
     # s2 = net.addSwitch('s2')
 
     # Links (ajustados para os novos nomes)
-    net.addLink(dana_main, s1, delay='1ms', loss=1, r2q=500)
-    net.addLink(remote1, s1, delay='1ms', loss=2, r2q=500)
-    net.addLink(remote2, s1, delay='1ms', loss=2, r2q=500)
+    net.addLink(dana_main, s1, delay='1ms')
+    net.addLink(remote1, s1, delay='1ms')
+    net.addLink(remote2, s1, delay='1ms')
     # net.addLink(locust, s1, bw=15, delay='2ms', loss=0.5, r2q=500)
     # net.addLink(serial, s2, bw=20, delay='20ms', loss=0.1, r2q=500)
     # net.addLink(locust_serial, s2, bw=15, delay='2ms', loss=0.5, r2q=500)
