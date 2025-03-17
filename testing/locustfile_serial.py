a2687f07d89be16a70ca86d84b1aa5126b555db7
Arthur Barata <arthurpbarata@gmail.com>
2025-03-17 18:26:19 -0300

    add mininet network, fix remote components address

---
+++ testing/locustfile_serial.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between
+import random
+
+class StressTestUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def stress_test(self):
+        size = 4
+        lines_A = ",".join([f"[{','.join(map(str, [random.randint(0, 10) for _ in range(size)]))}]" for _ in range(size)])
+        lines_I = ",".join([f"[{','.join(['1' if i == j else '0' for i in range(size)])}]" for j in range(size)])
+        A = f"[{lines_A}]"
+        I = f"[{lines_I}]"
+
+        with self.client.post("/matmul",
+                              json={"A": A, "B": I},
+                              catch_response=True) as response:
+
+            if response.text == '{"resultado": ' + A +'}':
+                response.success()
+            else:
+                response.failure("Wrong response")
---
+++ testing/mininet_network.py
@@ -0,0 +1,61 @@
+from mininet.net import Mininet
+from mininet.node import Controller, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+import time
+
+def test_network():
+    net = Mininet(controller=Controller, switch=OVSSwitch)
+    net.addController('c0')
+
+    switch = net.addSwitch('s1')
+    dana = net.addHost('dana', ip='10.5.0.2/16')
+    remote1 = net.addHost('remote1', ip='10.5.0.3/16')
+    remote2 = net.addHost('remote2', ip='10.5.0.4/16')
+    locust = net.addHost('locust', ip='10.5.0.5/16')
+
+    switch2 = net.addSwitch('s2')
+    serial = net.addHost('serial', ip='10.5.0.6/16')
+    locusts = net.addHost('locusts', ip='10.5.0.7/16')
+
+    net.addLink(dana, switch, delay="1ms")
+    net.addLink(remote1, switch, delay="1ms")
+    net.addLink(remote2, switch, delay="1ms")
+    net.addLink(locust, switch, delay="6ms")
+
+    net.addLink(serial, switch2, delay="5ms")
+    net.addLink(locusts, switch2, delay="20ms")
+
+    net.start()
+
+    # Inicie os serviços com logs
+    print("Iniciando serviços...")
+    dana.cmd('export DANA_HOME=/home/arthurb/dana_lang && export PATH=$PATH:$DANA_HOME')
+    remote1.cmd('export DANA_HOME=/home/arthurb/dana_lang && export PATH=$PATH:$DANA_HOME')
+    remote2.cmd('export DANA_HOME=/home/arthurb/dana_lang && export PATH=$PATH:$DANA_HOME')
+
+    dana.cmd('dana main.o 1 > dana.log 2>&1 &')
+    remote1.cmd('dana RemoteRepo.o 8081 2010 > remote1.log 2>&1 &')
+    remote2.cmd('dana RemoteRepo.o 8082 2011 > remote2.log 2>&1 &')
+
+    # Aguarde 10 segundos para serviços inicializarem
+    time.sleep(10)
+
+    # Teste a conectividade
+    print("Testando ping entre componentes:")
+    net.ping([dana, remote1], timeout=1)
+    net.ping([dana, remote2], timeout=1)
+    net.ping([locust, dana], timeout=1)
+    net.ping([locusts, serial], timeout=1)
+
+    # Execute o Locust
+    print("Iniciando teste Locust...")
+    locust.cmd("locust -f testing/locustfile.py --headless -u 500 -r 50 -H http://10.5.0.2:8080 --run-time 5m --csv results/dana &")
+    locusts.cmd("locust -f testing/locustfile_serial.py --headless -u 500 -r 50 -H http://10.5.0.6:5000 --run-time 5m --csv results/serial &")
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    test_network()
