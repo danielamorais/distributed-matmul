2a6d81f07046ea928e19c971697aada4e1d4cdb2
Daniela Morais <dani.m@hotmail.co.uk>
2025-12-03 23:11:17 -0300

    fix: rollback unnecesary changes

--- compile.sh
+++ compile.sh
@@ -2,14 +2,5 @@
 echo "Generating proxy..."
 python3 proxy_generator
 echo "Compiling Dana files..."
-# Compile only component files, excluding interfaces in resources/
-find . -name "*.dn" -type f \
-    ! -path "./resources/*" \
-    ! -path "./wasm_output/*" \
-    ! -path "./webserver/*" \
-    ! -path "./.git/*" \
-    ! -path "./results/*" \
-    ! -path "./testing/*" \
-    ! -path "./proxy_generator/*" \
-    -exec dnc {} \;
+dnc .
 echo "Compilation complete"
